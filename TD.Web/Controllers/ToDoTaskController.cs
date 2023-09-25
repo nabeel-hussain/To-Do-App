2ef98aa621cc3fbe70d7087321ffd2b120829751
Nabeel Hussain <luxkyshan@gmail.com>
2023-09-25 22:53:00 +0200

    Added New Web project and removed old project

---
+++ TD.Web/Controllers/ToDoTaskController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TD.Web.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class ToDoTaskController : ControllerBase
+    {
+        [HttpGet]
+        public Task<string> Index()
+        {
+            return Task.FromResult("test");
+        }
+    }
+}
--- TD.Web/Program.cs
+++ TD.Web/Program.cs
@@ -3,28 +3,26 @@ using TD.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllersWithViews();
 builder.Services.AddRepositories();
 builder.Services.AddDbConext(builder.Configuration);
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
 
+app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html");
+app.MapControllers();
 
 app.Run();
