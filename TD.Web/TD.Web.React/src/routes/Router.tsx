b214737b9a0b7039064ecff494240c3ebdd3bbc7
Nabeel Hussain <luxkyshan@gmail.com>
2023-09-30 18:44:28 +0200

    ddd

--- TD.Web/TD.Web.React/src/routes/Router.tsx
+++ TD.Web/TD.Web.React/src/routes/Router.tsx
@@ -2,11 +2,11 @@ import React, { lazy, Suspense } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
 import PageLoading from '../components/PageLoading';
-import Page from '../components/Page/Page';
+// import Page from '../components/Page/Page';
 
 import paths from '../routes/paths';
 
-const Home = lazy(async () => await import('../pages/Home/Home'));
+// const Home = lazy(async () => await import('../pages/Home/Home'));
 const NotFound = lazy(async () => await import('../pages/NotFound'));
 
 interface Routes {
@@ -16,14 +16,13 @@ interface Routes {
 
 const getRouteElement = (Component: React.ElementType): React.ReactNode => (
    <Suspense fallback={<PageLoading />}>
-      <Page>
+      <>
          <Component />
-      </Page>
+      </>
    </Suspense>
 );
 
 const routes: Routes[] = [
-   { path: paths.HOME, element: getRouteElement(Home) },
    { path: paths.NOT_FOUND, element: getRouteElement(NotFound) },
 ];
 const browserRoutes = createBrowserRouter(routes);
