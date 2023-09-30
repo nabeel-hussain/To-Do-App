87a1b416c2b1bfd08754b45d28325763ce07c1ac
Nabeel Hussain <luxkyshan@gmail.com>
2023-09-30 18:32:59 +0200

    fixed

--- TD.Web/TD.Web.React/vite.config.ts
+++ TD.Web/TD.Web.React/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'; 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base :"/",
   plugins: [react(),tsconfigPaths()],
 })
